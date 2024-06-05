2c78cdf0b53cd68347542ca1ee786a72bde630f2
Alumnos <Alumnos@VH209-23.upc.edu.pe>
2024-06-05 07:56:49 -0500

    Initial commit

---
+++ src/test/java/com/platform/tutorgo/TutorGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.tutorgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

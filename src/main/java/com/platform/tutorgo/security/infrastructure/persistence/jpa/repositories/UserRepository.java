ec9b729c4315dd3e00857515b2dd068f8b4a6060
Kurt Puican <u202016643@upc.edu.pe>
2024-06-23 01:35:48 -0500

    feat(secu):security infrastructure added

---
+++ src/main/java/com/platform/tutorgo/security/infrastructure/persistence/jpa/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.platform.tutorgo.security.infrastructure.persistence.jpa.repositories;
+
+import com.platform.tutorgo.security.domain.model.aggregates.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}

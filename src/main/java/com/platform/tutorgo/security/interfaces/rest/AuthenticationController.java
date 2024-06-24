34c886c2f9b0881bc409cdba03ca2d31450d56ec
RafaelLuyo <u201919295@upc.edu.pe>
2024-06-23 23:11:20 -0500

    feat(security): jpa auditing added

--- src/main/java/com/platform/tutorgo/security/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/platform/tutorgo/security/interfaces/rest/AuthenticationController.java
@@ -13,14 +13,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Authentication", description = "Authentication Management Endpoints")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
     private final UserCommandService userCommandService;
 
